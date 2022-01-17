bf142ddda8edf6f03f3ac4ad862c20218107d087
Han Y.B <yanbohan98@gmail.com>
2022-01-18 03:14:52 +0800

    [feat] A top bar layout with logo, buttons and user status(detail on going).

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -1,23 +1,38 @@
 <template>
-  <v-app-bar-title> HoaWiki</v-app-bar-title>
-  <v-app-bar-title>
-    {{ pagename }}
-  </v-app-bar-title>
-  <v-spacer></v-spacer>
-  <LocaleChanger></LocaleChanger>
+  <v-layout class=".flex-nowrap align-center" no-gutters="true">
+    <v-col class="d-flex justify-space-around flex-row align-center" xs="0">
+      <v-img :src="hoalogo" class="my-3" contain height="60" />
+      <v-app-bar-title> HoaWiki</v-app-bar-title>
+    </v-col>
+    <v-col>
+      <v-layout></v-layout>
+    </v-col>
+    <v-col class="d-flex justify-end flex-row">
+      <v-card>
+        <LocaleChanger></LocaleChanger>
+      </v-card>
+      <v-card>
+        <UserPanel></UserPanel>
+      </v-card>
+      <v-card class="bg-transparent" elevation="0" min-width="50"></v-card>
+    </v-col>
+  </v-layout>
 </template>
 
 <script>
 import LocaleChanger from "@/components/LocaleChanger.vue";
+import UserPanel from "@/components/UserPanel/UserPanel.vue";
+import hoalogo from "@/assets/hoalogo.jpg";
 
 export default {
   name: "AppBar",
   components: {
+    UserPanel,
     LocaleChanger,
   },
   data() {
     return {
-      pagename: "",
+      hoalogo,
     };
   },
 };
---
+++ src/components/UserPanel/UserPanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props">
+        <v-icon> mdi-account</v-icon>
+      </v-btn>
+    </template>
+    <div>
+      <v-card right top>
+        <v-list>
+          <v-list-item v-for="item in items" :key="item.title">
+            <v-icon>{{ item.icon }}</v-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </div>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: "UserPanel",
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: "Home", icon: "mdi-home-city" },
+        { title: "My Account", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" },
+      ],
+    };
+  },
+};
+</script>
