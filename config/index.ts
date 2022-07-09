d85aa6a2e28dae618eb0fd1d32e815a8c3948b0d
Han Y.B <yanbohan98@gmail.com>
2022-07-09 19:09:55 +0800

    ci(deployurltest): test the deploy url.

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,9 @@ export default {
     baseUrl: "http://localhost:8085/api", // 开发接口域名
   },
   beta: {
-    baseUrl:
-      "https://hoawiki-backend-1881225-1256984871.ap-shanghai.run.tcloudbase.com/api", // 测试接口域名
+    baseUrl: "wp.saltyball.top/api", // 测试接口域名
   },
   production: {
-    baseUrl:
-      "https://hoawiki-backend-1881225-1256984871.ap-shanghai.run.tcloudbase.com/api", // 正式接口域名
+    baseUrl: "wp.saltyball.top/api", // 正式接口域名
   },
 };
